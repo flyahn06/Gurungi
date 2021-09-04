5587ef519ee6600981699d5a4b509e47fc9090f3
DeVar <flyahn06@gmail.com>
2021-09-04 16:57:11 +0000

    src/gmethods/errors.cpp 작동 테스트

--- src/Gurungi.cpp
+++ src/Gurungi.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
 #include "lexer/lexer.hpp"
+#include "gmethods/errors.hpp"
 
 int main() {
+	Error *error = new Error("SomeBadException");
 	std::cout << "Hello, world!" << std::endl;
+	error_exit(error, "some bad exception occured!");
+	std::cout << "This message cannot be printed!" << std::endl;
 	return 0;
 }
