5a3ee1fcc0e648bc066ae8301375e493ed7fb76f
DeVar <flyahn06@gmail.com>
2021-09-04 16:58:21 +0000

    TokenType 생성 및 초기화 작성

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -1,1 +1,14 @@
 #include "lexer.hpp"
+
+TokenKind TokenType[256];
+
+void initTokenType(){
+	// TokenType 배열을 초기화합니다.
+	int i;
+	
+	for(i=0; i<256; i++)    TokenType[i]     = OTHERS;
+	for(i='0'; i<='9'; i++) TokenType[i]     = DIGIT;
+	for(i='A'; i<'Z'; i++)  TokenType[i]     = LETTER;
+	for(i='a'; i<'z'; i++)  TokenType[i]     = LETTER;
+	
+}
