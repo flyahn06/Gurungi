f3e7084aa63ada5221fb9e7ae7e1e0f44f6dca1f
DeVar <flyahn06@gmail.com>
2021-09-04 16:58:00 +0000

    기본 타입 작성

---
+++ src/gtypes/gtypes.hpp
@@ -0,0 +1,8 @@
+enum TokenKind {
+	RBRK_1='(', RBRK_2=')', BRAC_1 = '{', BRAC_2 = '}', SBRK_1 = '[', SBRK_2 = ']',
+	PLUS='+', MINUS='-', STAR='*', SLASH='/',   // 연산자 종류
+	
+	DIGIT, LETTER,  // 어휘 분석 시 숫자와 문자로 나누어 분석하기 때문에 필요합니다.
+	
+	IDENTIFIER, NUMBER, STRING, OTHERS  // 어휘 분석 후 토큰에 부여되는 타입ㅇ
+};
