37c3dfc4503d052ced8ea0884bbe2500f97b2fa7
DeVar <flyahn06@gmail.com>
2021-09-16 14:56:34 +0000

    initCharType 수정

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -1,14 +1,21 @@
 #include "lexer.hpp"
 
-TokenKind TokenType[256];
+TokenKind charType[256];
 
-void initTokenType(){
-	// TokenType 배열을 초기화합니다.
+using namespace std;
+
+void initCharType(){
+	// CharType 배열을 초기화합니다.
 	int i;
 	
-	for(i=0; i<256; i++)    TokenType[i]     = OTHERS;
-	for(i='0'; i<='9'; i++) TokenType[i]     = DIGIT;
-	for(i='A'; i<'Z'; i++)  TokenType[i]     = LETTER;
-	for(i='a'; i<'z'; i++)  TokenType[i]     = LETTER;
+	for(i=0; i<256; i++)    charType[i]     = OTHERS;
+	for(i='0'; i<='9'; i++) charType[i]     = DIGIT;
+	for(i='A'; i<'Z'; i++)  charType[i]     = LETTER;
+	for(i='a'; i<'z'; i++)  charType[i]     = LETTER;
+	
+	charType['_'] = LETTER;
+	charType['('] = RBRK_1; charType[')'] = RBRK_2;
+	charType['{'] = BRAC_1; charType['}'] = BRAC_2;
+	charType['['] = SBRK_1; charType[']'] = SBRK_2;
 	
 }
