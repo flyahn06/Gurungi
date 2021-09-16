b6bb32e4a5ade61cba923d495e2b5025b27ea111
DeVar <flyahn06@gmail.com>
2021-09-16 14:56:09 +0000

    인수 관련 작성

--- src/Gurungi.cpp
+++ src/Gurungi.cpp
@@ -3,10 +3,18 @@
 #include "lexer/lexer.hpp"
 #include "utils/errors.hpp"
 
-int main() {
-	Error *error = new Error("SomeBadException");
-	std::cout << "Hello, world!" << std::endl;
-	error_exit(error, "some bad exception occured!");
-	std::cout << "This message cannot be printed!" << std::endl;
+int main(int argc, char* args[]) {
+	if (argc == 1) {
+		error_exit("오류: 파일 이름이 없습니다. 자세한 내용은 \"Gurungi --도움\"을 참조하세요!");
+	}
+	
+	std::string filename = args[1];
+	
+	std::cout << filename << " 파일을 인터프릿합니다." << std::endl
+		<< "어휘 분석을 실행하는 중입니다...";
+	
+	
+	
+
 	return 0;
 }
