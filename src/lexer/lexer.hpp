e3c69c575ad7fb66c8544c3cb07961b7c9b62092
DeVar <flyahn06@gmail.com>
2021-09-20 16:48:17 +0900

    구조 수정

--- src/lexer/lexer.hpp
+++ src/lexer/lexer.hpp
@@ -1,8 +1,10 @@
 #include "../types/gtypes.hpp"
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <string>
 #include <cstdlib>
+#include <cstring>
 #include <cctype>
 
 void lexAnalysis(const std::string& filename);
