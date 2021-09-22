9391a9503e8646d728882e868b44f7b8df1db8bc
DeVar <flyahn06@gmail.com>
2021-09-22 18:26:57 +0000

    예외 표기 방식 수정

--- src/utils/errors.cpp
+++ src/utils/errors.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+Error* LexerError = new Error("어휘 분석 오류");
+Error* FuncDeclareError = new Error("함수 정의 오류");
+Error* VarDeclareError = new Error("변수 정의 오류");
+
+
 string Error::getErrorName() {
 	return errorName;
 }
@@ -14,12 +19,20 @@ Error::Error(const string& e_name) {
 	errorName = e_name;
 }
 
-void error_exit(string msg) {
-	cout << msg << endl;
+void error_exit(const string& msg) {
+	cerr << RED << "오류! " << RESET
+		 << msg << endl;
+	exit(1);
+}
+
+void error_exit(const string& tempErrorName, const string& msg) {
+	cerr << RED << tempErrorName << RESET << ": "
+		 << msg << endl;
 	exit(1);
 }
 
 void error_exit(Error *error, const string& msg) {
-	cout << error->getErrorName() << ": " << msg << endl;
+	cerr << RED << error->getErrorName() << RESET << ": " 
+		 << msg << endl;
 	exit(1);
 }
