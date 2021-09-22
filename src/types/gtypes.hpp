559cd351d89fd084626e86667f44e9179456f0df
DeVar <flyahn06@gmail.com>
2021-09-22 15:49:29 +0000

    여러 정의 통합

--- src/types/gtypes.hpp
+++ src/types/gtypes.hpp
@@ -1,5 +1,9 @@
 #include <string>
 
+#define KEYCOUNT 23
+#ifndef _GTYPES_HPP_
+#define _GTYPES_HPP_
+
 enum TokenKind {
 	RBRK_1=1, RBRK_2, BRAC_1, BRAC_2, SBRK_1, SBRK_2,
 	
@@ -20,44 +24,30 @@ enum TokenKind {
 
 };
 
-
-
-
-// struct Token {
-// 	TokenKind kind;
-// 	std::string text;
-// 	int intValue;
-// 	double doubleValue;
+struct Token {
+	TokenKind kind;
+	std::string text;
+	int intValue;
+	double doubleValue;
 	
-// 	Token() {
-// 		kind = OTHERS;
-// 		text = "";
-// 		intValue = 0;
-// 	}
-// 	Token(TokenKind k, const std::string& s, int d=0) {
-// 		kind = k;
-// 		text = s;
-// 		intValue = 0;
-// 	}
+	Token() {
+		kind = OTHERS;
+		text = "";
+		intValue = 0;
+	}
+	Token(TokenKind k, const std::string& s, int d=0) {
+		kind = k;
+		text = s;
+		intValue = 0;
+	}
 	
-// };
+};
 
-// struct KeyWord {
-// 	std::string keywordName;
-// 	TokenKind keywordKind;
-// };
+struct KeyWord {
+	std::string keywordName;
+	TokenKind keywordKind;
+};
 
-// KeyWord keyWordTable[] = {
-// 	{"만약", IF}, {"아니라면", ELSE},
-// 	{"끝", END}, {"프린트", PRINT},
-// 	{"(", RBRK_1}, {")", RBRK_2},
-// 	{"{", BRAC_1}, {"}", BRAC_2},
-// 	{"[", SBRK_1}, {"]", SBRK_2},
-// 	{"+", PLUS}, {"-", MINUS},
-// 	{"*", STAR}, {"/", SLASH},
-// 	{"=", ASSIGN}, {",", COMMA},
-// 	{"==", EQUAL}, {"/=", NOTEQUAL},
-// 	{"<", LESS}, {">", GREAT},
-// 	{"<=", LESSEQ}, {">=", GREATEQ},
-// 	{"\"", QUOTE}, {"", END_LIST}
-// };
+#endif
+extern KeyWord keyWordTable[KEYCOUNT];
+extern Token token;
