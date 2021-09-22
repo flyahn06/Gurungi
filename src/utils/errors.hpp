91be2dd453fa1317920ccc2e1baa397c2a6b4f85
DeVar <flyahn06@gmail.com>
2021-09-22 15:49:11 +0000

    다중 정의 오류 해결

--- src/utils/errors.hpp
+++ src/utils/errors.hpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <cstdlib>
 
+#ifndef _ERRORS_HPP_
+#define _ERRORS_HPP_
+
 
 class Error{
 	private:
@@ -12,6 +15,8 @@ class Error{
 		Error(const std::string& e_name);
 		std::string getErrorName();
 };
+
+#endif
 
 
 void error_exit(std::string msg);
